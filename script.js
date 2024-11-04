99a41ffcceba9fee0a8c9515bee7f08443b54ff3
unrelentin <augmentedvisionjcd@gmail.com>
2024-11-04 12:25:11 +0700

    added function to play one round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice (comChoice) {
     let random = Math.floor(Math.random() * 3)
         if (random == 0) {
@@ -7,7 +10,33 @@ function getComputerChoice (comChoice) {
         } else {
             comChoice = "Scissor";
         }
-    return comChoice;
+    return comChoice.toLowerCase();
 };
 
-console.log(getComputerChoice());
+function getHumanChoice (humanChoice) {
+    humanChoice = prompt("Rock, Paper or Scissor?", "no choice");
+    return humanChoice.toLowerCase();
+};
+
+function playRound (humanChoice, comChoice) {
+    if (humanChoice === comChoice) {
+        return console.log(`Draw! Computer chose ${comChoice}! The same as you!`)
+    } else if ((humanChoice === "rock" && comChoice === "paper") || (humanChoice === "paper" && comChoice === "scissor") || (humanChoice === "scissor" && comChoice === "rock")) {
+        computerScore ++
+        return console.log(`You lose! ${comChoice} beats ${humanChoice}!`);
+    } else if ((humanChoice === "rock" && comChoice === "scissor") || (humanChoice === "paper" && comChoice === "rock") || (humanChoice === "scissor" && comChoice === "paper")) {
+        humanScore ++
+        return console.log(`You win! ${humanChoice} beats ${comChoice}!`);
+    } else {
+        return console.log("You gave me no choice.")
+    }
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(computerSelection);
+
+
