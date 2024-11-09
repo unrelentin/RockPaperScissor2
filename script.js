dc9c3a7eb6943c37bcb70578950ae25ff2c3e45e
unrelentin <augmentedvisionjcd@gmail.com>
2024-11-09 14:13:06 +0700

    Added function to play five rounds

--- script.js
+++ script.js
@@ -13,30 +13,47 @@ function getComputerChoice (comChoice) {
     return comChoice.toLowerCase();
 };
 
-function getHumanChoice (humanChoice) {
-    humanChoice = prompt("Rock, Paper or Scissor?", "no choice");
-    return humanChoice.toLowerCase();
+function getHumanChoice () {
+    let choice= prompt("Rock, Paper or Scissor?", "no choice");
+    return choice.toLowerCase();
 };
 
 function playRound (humanChoice, comChoice) {
+    humanChoice = getHumanChoice();  // using == cause an error because == is comparison and = is assignment // 
+    comChoice = getComputerChoice();
+    console.log(`${comChoice}`);
+    
     if (humanChoice === comChoice) {
-        return console.log(`Draw! Computer chose ${comChoice}! The same as you!`)
+        console.log(`Draw! Computer chose ${comChoice}! The same as you! ${humanScore}:${computerScore}.`)
     } else if ((humanChoice === "rock" && comChoice === "paper") || (humanChoice === "paper" && comChoice === "scissor") || (humanChoice === "scissor" && comChoice === "rock")) {
         computerScore ++
-        return console.log(`You lose! ${comChoice} beats ${humanChoice}!`);
+        console.log(`You lose! ${comChoice} beats ${humanChoice}!${humanScore}:${computerScore}.`);
     } else if ((humanChoice === "rock" && comChoice === "scissor") || (humanChoice === "paper" && comChoice === "rock") || (humanChoice === "scissor" && comChoice === "paper")) {
         humanScore ++
-        return console.log(`You win! ${humanChoice} beats ${comChoice}!`);
+        console.log(`You win! ${humanChoice} beats ${comChoice}! ${humanScore}:${computerScore}.`);
     } else {
-        return console.log("You gave me no choice.")
+        console.log("You gave me no choice.")
     }
 };
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+        playRound();
+        playRound();
+        playRound();
+        playRound();
+        playRound();
+    if (humanScore > computerScore) {
+        return console.log(`You are the winner! ${humanScore}:${computerScore}.`)
+    } else if (humanScore < computerScore) {
+        return console.log(`You are the loser! ${humanScore}:${computerScore}.`)
+    } else {
+        return console.log(`Draw! ${humanScore}:${computerScore}.`)
+    }
+}
 
-playRound(humanSelection, computerSelection);
+playGame();
 
-console.log(computerSelection);
+
+
 
 
