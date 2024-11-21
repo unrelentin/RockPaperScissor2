a3d0631daa3ecffbe8fbf1edbeb8ed72d9ec56f2
unrelentin <augmentedvisionjcd@gmail.com>
2024-11-21 11:04:24 +0700

    added UI

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let roundCount = 0;
 
 function getComputerChoice (comChoice) {
     let random = Math.floor(Math.random() * 3)
@@ -13,45 +14,49 @@ function getComputerChoice (comChoice) {
     return comChoice.toLowerCase();
 };
 
-function getHumanChoice () {
-    let choice= prompt("Rock, Paper or Scissor?", "no choice");
-    return choice.toLowerCase();
-};
+const resultDisplay = document.querySelector(".resultDisplay");
+const finalResult = document.querySelector(".finalResult");
 
 function playRound (humanChoice, comChoice) {
-    humanChoice = getHumanChoice();  // using == cause an error because == is comparison and = is assignment // 
-    comChoice = getComputerChoice();
-    console.log(`${comChoice}`);
-    
-    if (humanChoice === comChoice) {
-        console.log(`Draw! Computer chose ${comChoice}! The same as you! ${humanScore}:${computerScore}.`)
-    } else if ((humanChoice === "rock" && comChoice === "paper") || (humanChoice === "paper" && comChoice === "scissor") || (humanChoice === "scissor" && comChoice === "rock")) {
-        computerScore ++
-        console.log(`You lose! ${comChoice} beats ${humanChoice}!${humanScore}:${computerScore}.`);
-    } else if ((humanChoice === "rock" && comChoice === "scissor") || (humanChoice === "paper" && comChoice === "rock") || (humanChoice === "scissor" && comChoice === "paper")) {
-        humanScore ++
-        console.log(`You win! ${humanChoice} beats ${comChoice}! ${humanScore}:${computerScore}.`);
+    comChoice = getComputerChoice(); //why must it get assign here? Because we need a fresh choice each time it's run. Make it local and not relying on global variables
+    if (roundCount == 5) {
+        scoreCount();
     } else {
-        console.log("You gave me no choice.")
+        if (humanChoice === comChoice) {
+            roundCount ++
+            resultDisplay.textContent = `Draw! Computer chose ${comChoice}! The same as you! ${humanScore}:${computerScore}.`
+        } else if ((humanChoice === "rock" && comChoice === "paper") || (humanChoice === "paper" && comChoice === "scissor") || (humanChoice === "scissor" && comChoice === "rock")) {
+            computerScore ++
+            roundCount ++
+            resultDisplay.textContent = `You lose! ${comChoice} beats ${humanChoice}!${humanScore}:${computerScore}.`;
+        } else if ((humanChoice === "rock" && comChoice === "scissor") || (humanChoice === "paper" && comChoice === "rock") || (humanChoice === "scissor" && comChoice === "paper")) {
+            humanScore ++
+            roundCount ++
+            resultDisplay.textContent = `You win! ${humanChoice} beats ${comChoice}! ${humanScore}:${computerScore}.`;
+        } else {
+            resultDisplay.textContent = "You gave me no choice.";
+        }
     }
 };
 
-function playGame() {
-        playRound();
-        playRound();
-        playRound();
-        playRound();
-        playRound();
+const rockButton = document.querySelector(".rockButton");
+      rockButton.addEventListener("click", () => playRound("rock"));
+const paperButton = document.querySelector(".paperButton");
+      paperButton.addEventListener("click", () => playRound("paper"));
+const scissorButton = document.querySelector(".scissorButton");
+      scissorButton.addEventListener("click", () => playRound("scissor"));
+
+function scoreCount() {                       //Remove humanScore and computerScore as parameter in playGame() because it overides the global parameter humanScore and computerScore causing it not get updated properly
     if (humanScore > computerScore) {
-        return console.log(`You are the winner! ${humanScore}:${computerScore}.`)
+        finalResult.textContent = `You are the winner!`;
     } else if (humanScore < computerScore) {
-        return console.log(`You are the loser! ${humanScore}:${computerScore}.`)
+        finalResult.textContent = `You are the loser!`;
     } else {
-        return console.log(`Draw! ${humanScore}:${computerScore}.`)
+        finalResult.textContent = `Draw!`;
     }
 }
 
-playGame();
+//If I want to end the game when one player reaches 5 points all I need to do is check if one player has reached five points or not and then call scoreCount()
 
 
 
